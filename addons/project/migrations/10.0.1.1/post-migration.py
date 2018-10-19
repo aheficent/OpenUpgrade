f78b5ad9dda895a69cfecc8fc772be62b70e32bb
aheficent <ahenriquez@eficent.com>
2018-10-19 14:11:07 +0200

    [IMP]do not archive projects. disagree with openupgrade

--- addons/project/migrations/10.0.1.1/post-migration.py
+++ addons/project/migrations/10.0.1.1/post-migration.py
@@ -7,12 +7,6 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
-    openupgrade.logged_query(
-        env.cr,
-        """UPDATE project_project
-        SET active=False
-        WHERE state IN ('cancelled', 'close')"""
-    )
     openupgrade.load_data(
         env.cr, 'project', 'migrations/10.0.1.1/noupdate_changes.xml',
     )
